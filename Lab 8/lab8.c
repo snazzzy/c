5ecb8d2e33060b0286b88e7ac9c1f93e4e55e10a
John Traner <jatraner@gmail.com>
2018-12-06 20:45:44 -0800

    Add files via upload

---
+++ Lab 8/lab8.c
@@ -0,0 +1,9 @@
+int main() {
+
+int a = 0;
+int b = 0;
+a = a + b;
+b = b + a;
+b = b + 1;
+
+}
---
+++ Lab 8/lab8p3.c
@@ -0,0 +1,17 @@
+int main() {
+//part2
+  int array[13];
+  array[13] = 0; //g
+  array[12] = 23; //a
+  array[11] = 19; //b
+  array[10] = 12; //c
+  array[7] = 3; //e
+  array[6] = 1; //f
+  array[8] = 2; //d
+
+   array[13] = array[12] * array[8];
+   array[13] = array[11] * array[7];
+   array[13] = array[10] * array[6];
+
+  //array[13] += ((array[12] * array[8])) + ((array[11] * array[7])) + ((array[10] * array[6]));
+}
